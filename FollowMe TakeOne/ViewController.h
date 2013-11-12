7a50b3e3466fb499542deefa5413cf29b13a081a
Routing Developer <dev@Pablos-MacBook-Pro.local>
2013-11-12 00:50:47 -0300

    Initial Commit

---
+++ FollowMe TakeOne/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FollowMe TakeOne
+//
+//  Created by Routing Developer on 11/12/13.
+//  Copyright (c) 2013 Routing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FollowMe TakeOneTests/FollowMe_TakeOneTests.m
@@ -0,0 +1,34 @@
+//
+//  FollowMe_TakeOneTests.m
+//  FollowMe TakeOneTests
+//
+//  Created by Routing Developer on 11/12/13.
+//  Copyright (c) 2013 Routing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FollowMe_TakeOneTests : XCTestCase
+
+@end
+
+@implementation FollowMe_TakeOneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

df046b85b6beb03cbbfd3baa97633b7077b451ba
Routing Developer <dev@Pablos-MacBook-Pro.local>
2013-11-14 20:42:34 -0300

    First version, captures position every once in a while and shows it on screen.

--- FollowMe TakeOne/ViewController.h
+++ FollowMe TakeOne/ViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface ViewController : UIViewController
+- (IBAction)followMeToggled:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextView *statusTextView;
+@property (weak, nonatomic) IBOutlet UISwitch *toggleButton;
+@property (strong, nonatomic) IBOutlet CLLocationManager *locationManager;
+
 
 @end
--- FollowMe TakeOne/ViewController.m
+++ FollowMe TakeOne/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest; // 100 m
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void) addTextToState:(NSString *)message
+{
+    NSString* oldText = [@"\n" stringByAppendingString:self.statusTextView.text];
+    NSString* newText = [message stringByAppendingString:oldText];
+    [self.statusTextView setText:newText];
+
+}
+
+- (IBAction)followMeToggled:(id)sender {
+    
+    [self addTextToState:@"Switch toggled"];
+    if(self.toggleButton.isOn){
+        [self addTextToState:@"Is on"];
+        [_locationManager startUpdatingLocation];
+    }else
+    {
+        [self addTextToState:@"Is off"];
+        [_locationManager stopUpdatingLocation];
+    }
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations {
+    // If it's a relatively recent event, turn off updates to save power.
+    CLLocation* location = [locations lastObject];
+    NSDate* eventDate = location.timestamp;
+    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+    if (abs(howRecent) < 15.0) {
+        // If the event is recent, do something with it.
+        NSString *lat = [NSString stringWithFormat:@"%f", location.coordinate.latitude];
+        NSString *lng = [NSString stringWithFormat:@"%f", location.coordinate.longitude];
+        NSString *spd = [NSString stringWithFormat:@"%f", location.speed];
+        
+        NSString *statusUpdate = [[[[lat stringByAppendingString:@","] stringByAppendingString:lng] stringByAppendingString:@" at "]stringByAppendingString:spd];
+        
+        [self addTextToState:statusUpdate];
+    }
+}
+
 
 @end

8190b221979c349c026b80d7fcacd8453e1157dc
Routing Developer <dev@Pablos-MacBook-Pro.local>
2013-11-15 01:04:36 -0300

    Generates JSON string and sends it to routing epic

--- FollowMe TakeOne/ViewController.m
+++ FollowMe TakeOne/ViewController.m
@@ -21,7 +21,55 @@
     _locationManager = [[CLLocationManager alloc] init];
     _locationManager.delegate = self;
     _locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
-    _locationManager.desiredAccuracy = kCLLocationAccuracyBest; // 100 m
+    _locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+}
+
+- (NSDictionary*) packLocationInDictionary:(CLLocation*) location
+{
+    NSMutableDictionary* dict = [[NSMutableDictionary alloc] init];
+    
+    NSString *lat = [NSString stringWithFormat:@"%f", location.coordinate.latitude];
+    NSString *lng = [NSString stringWithFormat:@"%f", location.coordinate.longitude];
+    NSString *spd = [NSString stringWithFormat:@"%2.2f", location.speed];
+    NSString *acc = [NSString stringWithFormat:@"%2.2f", location.horizontalAccuracy];
+
+    [dict setValue:lat forKey:@"latitude"];
+    [dict setValue:lng forKey:@"longitude"];
+    [dict setValue:spd forKey:@"speed"];
+    [dict setValue:acc forKey:@"accuracy"];
+    [dict setValue:@"Follower-IOS-1" forKey:@"version"];
+    NSString* sender = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    [dict setValue:sender forKey:@"sender"];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    NSString *formattedDateString = [dateFormatter stringFromDate:location.timestamp];
+
+    [dict setValue:formattedDateString forKey:@"timestamp"];
+    
+    return dict;
+}
+
+- (NSString*) convertDictionaryToJSON: (NSDictionary*) dict
+{
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict
+                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                         error:&error];
+    NSString *jsonString = @"{}";
+    if (! jsonData) {
+        NSLog(@"Got an error: %@", error);
+    } else {
+        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+    return jsonString;
+}
+
+- (NSString *) convertLocationToJSON:(CLLocation*) location
+{
+    NSDictionary* dict = [self packLocationInDictionary:location];
+    NSString* json = [self convertDictionaryToJSON:dict];
+    
+    return json;
 }
 
 - (void)didReceiveMemoryWarning
@@ -51,6 +99,22 @@
     
 }
 
+- (void)sendLocationToServer:(NSString*) jsonRequest
+{
+    NSData *requestData = [NSData dataWithBytes:[jsonRequest UTF8String] length:[jsonRequest length]];
+    NSURL* url = [NSURL URLWithString:@"http://api.routing.uc.cl/trackpoints"];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:[NSString stringWithFormat:@"%d", [requestData length]] forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody: requestData];
+    
+    NSURLConnection *connection = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+    [connection start];
+    
+}
+
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations {
     // If it's a relatively recent event, turn off updates to save power.
@@ -59,13 +123,14 @@
     NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
     if (abs(howRecent) < 15.0) {
         // If the event is recent, do something with it.
-        NSString *lat = [NSString stringWithFormat:@"%f", location.coordinate.latitude];
-        NSString *lng = [NSString stringWithFormat:@"%f", location.coordinate.longitude];
-        NSString *spd = [NSString stringWithFormat:@"%f", location.speed];
         
-        NSString *statusUpdate = [[[[lat stringByAppendingString:@","] stringByAppendingString:lng] stringByAppendingString:@" at "]stringByAppendingString:spd];
+        NSString *pos = [NSString stringWithFormat:@"{%f,%f}@%2.2f[m/s] (%2.2f)", location.coordinate.latitude,location.coordinate.longitude,location.speed, location.horizontalAccuracy];
         
-        [self addTextToState:statusUpdate];
+        
+        NSString *newLocationJSON = [self convertLocationToJSON:location];
+
+        [self sendLocationToServer:newLocationJSON];
+        [self addTextToState:pos];
     }
 }
 
